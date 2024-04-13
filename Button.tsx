64cb43b85089bbd897d780caf5dd3b7d2522445f
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-04-13 13:47:43 +0530

    feat: Separate components for expo and web

--- Button.tsx
+++ Button.tsx
@@ -1,19 +1,25 @@
 'use client'
-import {Pressable } from "react-native";
-import styled from 'styled-components/native'
-import buttonStyles from "./buttonStyles";
-import ButtonText from "./ButtonText";
+import React from 'react';
+import styled from 'styled-components'
 
-const Custom = styled(Pressable)`
-  ${buttonStyles}
+const Button = styled.button`
+  border-radius: ${p => p.theme.borderRadius.md};
+  font-size: 0.9rem;
+  background: ${p => p.theme.colors.blue};
+  color: white;
+  padding: .6em 1.2em;
+  box-shadow: none;
+  border: none;
+  width: max-content;
+  transition: background-color .2s ease;
+  @media (prefers-reduced-motion) {
+    transition: none;
+  }
+ 
+  &:hover {
+    background: ${p => p.theme.colors.lightBlue};
+  } 
+  
 `
-
-const Button = (props) => {
-    return <Custom>
-        <ButtonText>
-            {props.children}
-        </ButtonText>
-    </Custom>
-}
 
 export default Button;
