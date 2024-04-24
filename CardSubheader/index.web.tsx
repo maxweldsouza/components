9e42cbcb0a9838620ef79051e49e834ee0a7f54e
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-04-24 22:44:01 +0530

    fix: Card

--- CardSubheader/index.web.tsx
+++ CardSubheader/index.web.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from "styled-components/native";
+import styled from "styled-components";
 import {Text} from 'react-native'
 import {IThemeProps} from "../theme";
 
