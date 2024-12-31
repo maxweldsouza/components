122c7a91fe6ad79658df10b71df0a75a25ee87da
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2024-12-31 10:49:29 +0530

    refactor: Better hook

--- hooks/useList.ts
+++ hooks/useList.ts
@@ -1,48 +1,67 @@
 import { useState } from "react";
 
-export function useList<T>(initialState: Array<T> = [] ) {
+// This is similar to react-use but uses updaterFns to prevent unnecessary renders
+
+export function useList<T>(initialState: Array<T> = [] ) : [Array<T>, any] {
   const [list, setList] = useState<Array<T>>(initialState);
 
-  return [
-    list,
-    {
-      set: (newList) => {
-        setList(newList);
-      },
-      filter: (predicate) => {
-        setList((list) => {
-          return list.filter(predicate);
-        });
-      },
-      push: (...items) => {
-        setList((list) => {
-          return [...list, ...items];
-        });
-      },
-      updateAt: (index, updaterFnOrObj) => {
-        setList((list) => {
-          return list.map((item, i) => {
-            if (i === index) {
-              if (typeof updaterFnOrObj === 'function') {
-                return updaterFnOrObj(item);
-              }
-              if (typeof updaterFnOrObj === 'object') {
-                return {
-                  ...item,
-                  ...updaterFnOrObj
-                }
-              }
-              return updaterFnOrObj
-            }
-            return item;
-          });
-        });
-      },
-      removeAt: (index) => {
-        setList((list) => {
-          return list.filter((item, i) => i !== index);
-        });
-      },
+  const updateList = {
+    set: (newList) => {
+      setList(newList);
     },
-  ];
+    filter: (predicate) => {
+      setList((list) => {
+        return list.filter(predicate);
+      });
+    },
+    push: (...items) => {
+      setList((list) => {
+        return [...list, ...items];
+      });
+    },
+    update: (updaterFnOrObj, predicate = (x, i) => true) => {
+      setList((list) => {
+        return list.map((item, i) => {
+          if (predicate(item, i)) {
+            if (typeof updaterFnOrObj === 'function') {
+              return updaterFnOrObj(item);
+            }
+            if (typeof updaterFnOrObj === 'object') {
+              return {
+                ...item,
+                ...updaterFnOrObj
+              }
+            }
+            return updaterFnOrObj
+          }
+          return item;
+        });
+      });
+    },
+    updateAt: (index, updaterFnOrObj) => {
+      setList((list) => {
+        return list.map((item, i) => {
+          if (i === index) {
+            if (typeof updaterFnOrObj === 'function') {
+              return updaterFnOrObj(item);
+            }
+            if (typeof updaterFnOrObj === 'object') {
+              return {
+                ...item,
+                ...updaterFnOrObj
+              }
+            }
+            return updaterFnOrObj
+          }
+          return item;
+        });
+      });
+    },
+    removeAt: (index) => {
+      setList((list) => {
+        return list.filter((item, i) => i !== index);
+      });
+    },
+  }
+  return [list, updateList];
 }
