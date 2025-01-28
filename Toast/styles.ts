dc21df2929156405025c042ed96c86bd1cb95312
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2025-01-28 11:00:19 +0530

    feat: Better toast

--- Toast/styles.ts
+++ Toast/styles.ts
@@ -1,8 +1,8 @@
-import { css } from 'styled-components';
+import { css } from "styled-components";
 
 export default css`
   position: fixed;
-  bottom: 30px;
+  bottom: 80px;
   left: 50%;
   color: ${(p) => p.theme.colors.background};
   background-color: ${(p) => p.theme.colors.text};
@@ -10,12 +10,14 @@ export default css`
   padding: ${(p) => p.theme.spacing.md};
   border-radius: ${(p) => p.theme.borderRadius.md};
   opacity: ${(p) => (p.$show ? 1 : 0)};
-  visibility: ${(props) => (props.$show ? 'visible' : 'hidden')};
+  visibility: ${(props) => (props.$show ? "visible" : "hidden")};
   transform-origin: 100% 0;
   transform: ${(props) =>
     props.$show
-      ? 'scale(1) translateY(0) translateX(-50%)'
-      : 'scale(1) translateY(100%) translateX(-50%)'};
-  transition: transform 0.2s ease-in-out, opacity 0.2s ease-in-out,
+      ? "scale(1) translateY(0) translateX(-50%)"
+      : "scale(1) translateY(100%) translateX(-50%)"};
+  transition:
+    transform 0.2s ease-in-out,
+    opacity 0.2s ease-in-out,
     visibility 0.2s;
 `;
